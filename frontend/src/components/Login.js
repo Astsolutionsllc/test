4c6fded2958af3a0c79167d5c7f4480904810537
Astsolutionsllc <customerservice@astsolutionsllc.com>
2025-03-10 08:56:41 -0700

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('http://44.208.28.102:5000/api/auth/login', {
         email,
         password,
       });
